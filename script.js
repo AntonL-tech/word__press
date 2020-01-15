5764819287843fd471316577b36a2f33533e6b28
Anton <antonlashyk@gmail.com>
2020-01-15 20:53:17 +0300

    init maket

---
+++ script.js
@@ -0,0 +1,7 @@
+var question = document.querySelector('#question');
+
+function greeting() {
+    var age = prompt('Введите свой возраст', '');
+    var result = age * 365;
+    console.log(result); 
+}
